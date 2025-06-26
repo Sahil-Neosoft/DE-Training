688a14420f945b6c4daaae7e139d35ddb84b9413
Sahil-Neosoft <sahil.patel@neosoftmail.com>
2025-06-26 17:31:35 +0530

    Delete DAY 9-12/app directory

--- DAY 9-12/app/orders/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, PositiveInt, Field
-from typing import List
-from datetime import datetime
-
-class OrderItem(BaseModel):
-    product_id: PositiveInt
-    name: str = Field(..., min_length=2, max_length=100)
-    quantity
